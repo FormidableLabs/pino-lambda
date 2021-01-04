d6ca5fac8c5df7945955cc160a08b7886bab7cb6
Charles Brown <carbonrobot@gmail.com>
2021-01-04 09:07:00 -0600

    test: add basic test coverage

---
+++ src/tests/index.spec.ts
@@ -0,0 +1,60 @@
+import { Context } from 'aws-lambda';
+import tap from 'tap';
+import sinon from 'sinon';
+import pino, { PinoLambdaLogger } from '../index';
+
+sinon.useFakeTimers(new Date(2016, 11, 1).getTime());
+
+tap.test('should log a simple info message', (t) => {
+  const [log, output] = createLogger();
+
+  log.info('Simple message');
+  t.matchSnapshot(output.buffer);
+  t.end();
+});
+
+tap.test('should log an info message with data', (t) => {
+  const [log, output] = createLogger();
+
+  log.info({ data: { stuff: 'unicorns' } }, 'Message with data');
+  t.matchSnapshot(output.buffer);
+  t.end();
+});
+
+tap.test('should log an info message with requestId', (t) => {
+  const [log, output] = createLogger();
+
+  log.withRequest({}, { awsRequestId: '12345' } as Context);
+  log.info('Message with request ID');
+  t.matchSnapshot(output.buffer);
+  t.end();
+});
+
+tap.test('should log an error message with requestId', (t) => {
+  const [log, output] = createLogger();
+
+  log.withRequest({}, { awsRequestId: '12345' } as Context);
+  log.error('Message with request ID');
+  t.matchSnapshot(output.buffer);
+  t.end();
+});
+
+/**
+ * Creates a test logger and output buffer for assertions
+ * Returns the logger and the buffer
+ */
+function createLogger(): [PinoLambdaLogger, { buffer: string }] {
+  const output = {
+    buffer: 'undefined',
+  };
+
+  const logger = pino({
+    streamWriter: (str: string | Uint8Array): boolean => {
+      output.buffer = (str as string).trim();
+      return true;
+    },
+    base: null,
+  });
+
+  return [logger, output];
+}
---
+++ tap-snapshots/src-tests-index.spec.ts-TAP.test.js
@@ -0,0 +1,22 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`src/tests/index.spec.ts TAP should log a simple info message > must match snapshot 1`] = `
+2016-12-01T06:00:00.000Z	undefined	INFO	Simple message	{"level":30,"time":1480572000000,"msg":"Simple message"}
+`
+
+exports[`src/tests/index.spec.ts TAP should log an error message with requestId > must match snapshot 1`] = `
+2016-12-01T06:00:00.000Z	12345	ERROR	Message with request ID	{"level":50,"time":1480572000000,"awsRequestId":"12345","x-correlation-id":"12345","msg":"Message with request ID"}
+`
+
+exports[`src/tests/index.spec.ts TAP should log an info message with data > must match snapshot 1`] = `
+2016-12-01T06:00:00.000Z	undefined	INFO	Message with data	{"level":30,"time":1480572000000,"data":{"stuff":"unicorns"},"msg":"Message with data"}
+`
+
+exports[`src/tests/index.spec.ts TAP should log an info message with requestId > must match snapshot 1`] = `
+2016-12-01T06:00:00.000Z	12345	INFO	Message with request ID	{"level":30,"time":1480572000000,"awsRequestId":"12345","x-correlation-id":"12345","msg":"Message with request ID"}
+`
